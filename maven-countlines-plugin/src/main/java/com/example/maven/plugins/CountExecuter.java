641d6d072f45229c0cab0471b3404e9af44eb529
yamaguchi <mic-yamaguchi@inter-mic.co.jp>
2013-12-27 19:45:34 +0900

    test

--- maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
+++ maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
@@ -42,8 +42,6 @@ public class CountExecuter {
 		System.out.println("bbb");
 		System.out.println("cccc");
 		System.out.println("dddd");
-		System.out.println("eeee");
-		System.out.println("ffff");
 		System.out.println("hhh");
 		System.out.println()
 	}
