faf3c2ccd04e92a20a6d6cfa228cc09a3aa7ee3e
yamaguchi <mic-yamaguchi@inter-mic.co.jp>
2013-12-27 19:23:20 +0900

    test

--- maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
+++ maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
@@ -45,7 +45,6 @@ public class CountExecuter {
 		System.out.println("1414");
 		System.out.println("1416");
 		System.out.println("1438");
-		System.out.println("1441");
 	}
 
 	private int countLines(File file) throws IOException {
