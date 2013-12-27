9820e4823289a12f918d232b25cccb1812ab9e41
Yamaguchi <Yamaguchi@192.168.101.114>
2013-12-27 16:09:27 +0900

    br

--- maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
+++ maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
@@ -52,6 +52,7 @@ public class CountExecuter {
 		System.out.println("1550");
 		System.out.println("1551");
 		System.out.println("1557");
+		System.out.println();
 	}
 
 	private int countLines(File file) throws IOException {
