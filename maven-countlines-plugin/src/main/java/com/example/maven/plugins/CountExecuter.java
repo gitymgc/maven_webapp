981b73cce5d5b354578d7bb4c9015839a7a9a611
Yamaguchi <Yamaguchi@192.168.101.114>
2013-12-27 09:51:57 +0900

    test

--- maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
+++ maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
@@ -39,6 +39,7 @@ public class CountExecuter {
 		System.out.println("directory " + directory.getPath());
 		System.out.println();
 		System.out.println("develop");
+		System.out.println("test");
 		
 		
 	}
