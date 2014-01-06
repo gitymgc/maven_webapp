d959aefabd3c222778531c50399bfacc548507b9
yamaguchi <mic-yamaguchi@inter-mic.co.jp>
2014-01-06 11:03:25 +0900

    1103

--- maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
+++ maven-countlines-plugin/src/main/java/com/example/maven/plugins/CountExecuter.java
@@ -44,6 +44,7 @@ public class CountExecuter {
 		System.out.println("dddd");
 		System.out.println("hhh");
 		System.out.println()
+		System.out.println("1103");
 		
 		
 		System.out.println("0932");
